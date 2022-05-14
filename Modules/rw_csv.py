3ccbfbdd76c4ed36d0a4ecbb60e0666f1a04061c
Vendetta <34066247+vendettadelsangue@users.noreply.github.com>
2022-05-14 23:14:25 +0900

    Full version updated

--- Modules/rw_csv.py
+++ Modules/rw_csv.py
@@ -8,7 +8,7 @@ else:
     from Modules.todoDataClass import todoData
 
 # functions declaration
-def writeData(todoData: todoData):
+def writeData(todoData: todoData | None):
     '''Add data to csv file.'''
     todoData = todoData.__dict__
     todoDatas = readData()
@@ -27,7 +27,7 @@ def writeData(todoData: todoData):
                         'description': todoData["description"], 'succeeded': todoData["succeeded"]})
 
 
-def readData(index=None):
+def readData(index: int | None = None):
     '''Return todo data'''
     with open('./todo.csv', newline='') as csvfile:
         reader = csv.DictReader(csvfile)
@@ -39,6 +39,30 @@ def readData(index=None):
             data = next((i for i in reader if i['index'] == str(index)), None)
             return data
 
+def delData(index: int=None):
+    '''Del data from csv file.'''
+    if not index:
+        with open('./todo.csv', 'w', newline='') as csvfile:
+            fieldnames = ['index', 'date', 'topic', 'description', 'succeeded']
+            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+
+            writer.writeheader()
+        pass
+    todoDatas = readData()
+    with open('./todo.csv', 'w', newline='') as csvfile:
+        fieldnames = ['index', 'date', 'topic', 'description', 'succeeded']
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+
+        writer.writeheader()
+        count = 1
+        if todoDatas:
+            for i in todoDatas:
+                if i["index"] == str(index):
+                    print(i)
+                    continue
+                writer.writerow({'index': count, 'date': i["date"], 'topic': i["topic"],
+                                'description': i["description"], 'succeeded': i["succeeded"]})
+                count += 1
 
 def getDataSize():
     '''Return todo data's size'''
@@ -49,7 +73,4 @@ def getDataSize():
 
 # Test Code
 if __name__ == '__main__':
-    # print(getDataSize())
-    # readData(1)
-    data = todoData("Test", "Write Data")
-    writeData(data)
+    delData(2)
--- app.py
+++ app.py
@@ -1,14 +1,13 @@
 # import Modules
-from Modules.rw_csv import writeData, readData, getDataSize
+from Modules.rw_csv import writeData, readData, getDataSize, delData
 from Modules.todoDataClass import todoData
 from Modules.consoleCleaner import clear
 from math import ceil
 
 # variable declaration
-done = False
 ment = None
 
-while not done:
+while True:
     clear()
     if ment:
         print("[" + ment + "]")
@@ -18,6 +17,8 @@ while not done:
 1. Write todo.
 2. Load all todo lists.
 3. Get information about a specific todo.
+4. Clear specific todo information.
+5. Clear todo list.
 e. Exit
 """, sep="")
 
@@ -34,21 +35,24 @@ e. Exit
                 writeData(todoData(topic, description) if description else todoData(topic))
                 ment = "topic %s is successfully added to the todo list" % topic
                 break
+
         case "2":
+            page = 1
             while True:
                 data = readData()
                 if not data:
-                    print("Data is not definded")
+                    ment = "Data is not definded"
                     break
                 datas = []
-                page = 1
                 pages = ceil(len(data) / 5)
                 print("This page is %d" % (page))
                 print("=" * 53)
                 print(format("index", "^5")," | ",format("topic", "^20")," | ",format("date", "^10")," | ",format("succeeded", "^9"), sep="")
                 print("=" * 53)
-                pageData = next((i for i in data if page <= int(i['index']) or page * 5 >= int(i['index']) ), None)
                 for i in data:
+                    if (page - 1) * 5 < int(i['index']) and (page-1) * 5 + 5 >= int(i['index']):
+                        datas.append(i)
+                for i in datas:
                     print(format(i["index"], "^5")," | ",format(i["topic"] if len(i["topic"]) < 20 else i["topic"][:18] + "..", "^20")," | ",format(i["date"], "^10")," | ",format(i["succeeded"], "^9"), sep="")
                 choice = input("Please enter page number(or n) : ")
                 try:
@@ -76,7 +80,43 @@ e. Exit
                     clear()
                     for i in data:
                         print(format(i,"<11"), ": ", data[i])
+
+        case "4":
+            while True:
+                choice = input("Please enter index number(or n) : ")
+                data = readData(choice)
+                if choice.lower() == "n":
+                    break
+                elif not data:
+                    print("Invaild index")
+                else:
+                    clear()
+                    for i in data:
+                        print(format(i,"<11"), ": ", data[i])
+                    num = choice
+                    print("\n\nAre you sure to delete the above information?")
+                    while True:
+                        choice = input("Y/N : ").lower()
+                        if choice == "y":
+                            delData(num)
+                            print("Successfully deleted")
+                            break
+                        elif choice == "n":
+                            print("Cancelled")
+                            break
+                        else:
+                            print("Wrong choice")
+
+        case "5":
+            choice = input("Are you sure? : (Y/N) : ").lower()
+            if choice == "y":
+                delData()
+                ment = "Successfully deleted all data"
+            else:
+                ment = "Cancelled"
+
         case "e":
             break
+
         case _:
             ment = "It's not the right choice."
